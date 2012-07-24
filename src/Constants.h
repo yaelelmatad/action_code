806b4bc2d4dfa629e8c639d56e5940e70cea7374
Ceri Stagg <ceristaggbusiness@gmail.com>
2012-07-24 17:35:15 -0400

    Added new file Constants.h

---
+++ src/Constants.h
@@ -0,0 +1,17 @@
+/*
+ *  config.h
+ *  Action
+ *
+ *  Created by Yael Elmatad on 8/16/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _CONSTANTS_H
+#define	_CONSTANTS_H
+
+#define MAXSIZE 10000
+#define NUM_LISTS 8
+#define MAXARGS 100
+
+#endif /*_CONSTANTS_H*/
