8e773a72eb5e64f738f81ff0cd85ffab6d07e67f
yaelelmatad <yael.elmatad@gmail.com>
2012-11-30 17:40:07 -0500

    moved mpi

--- src/main.cpp
+++ src/main.cpp
@@ -185,6 +185,152 @@ int main (int argc, char * const argv[]) {
 	{
 		myTPS.TPS_move(trajectory); //actually does the TPS stuff
 		trajectory.PrintOrderParameter(myTPS.GetCurrS()); //prints the order parameter
+        
+        /***********************MPI*************************/
+
+        
+        if (0==my_rank)
+        {
+            //shuffle the swapArray
+            //NOTE: YOU MUST MAKE SURE YOU SAVE AND RELOAD THIS INFORMATION FOR NEXT TIME YOU RUN THE SIMULATION
+            int firstSwap;
+            int secondSwap;
+            int temp;
+            
+            //set up the swap matrix  (redo each time to avoid having to save state of swap array in restart files)
+            for (int i=0; i<MAX_COMM_SZ;i++)
+            {
+                swapArray[i]=i;
+                n_swaps = comm_sz*2;
+            }
+            
+            //arbitrarily swap lots of times so you shuffle the array
+            for (int i = 0; i<n_swaps; i++)
+            {
+                firstSwap = rand()%comm_sz;
+                secondSwap = rand()%comm_sz;
+                
+                temp = swapArray[firstSwap];
+                swapArray[firstSwap]=swapArray[secondSwap];
+                swapArray[secondSwap]=temp;
+            }
+            
+            //determines who attempst to swap with whom.
+            for (int i = 0; i < comm_sz; i=i+2)
+            {
+                //send child/slave information for later swapping (only parent proc will handle decision making)
+                int child[1];
+                *child = 1;
+                int parent[1];
+                *parent = 0;
+                
+                //cout << swapArray[i] << " swaps with " << swapArray[i+1] << endl;
+                //cout << swapArray[i+1] << " swaps with " << swapArray[i] << endl;
+                
+                //temp stuff for stupid mpi
+                int swappers[1];
+                
+                //send the swapping information to all the nodes
+                
+                //send to the children (odd)
+                *swappers = swapArray[i];
+                MPI_Send(&swappers,1, MPI_INT, swapArray[i+1], 0, MPI_COMM_WORLD);
+                MPI_Send(&child,1, MPI_INT, swapArray[i+1], 1, MPI_COMM_WORLD);
+                
+                //send to parents (even)
+                *swappers = swapArray[i+1];
+                MPI_Send(&swappers,1, MPI_INT, swapArray[i], 0, MPI_COMM_WORLD);
+                MPI_Send(&parent,1, MPI_INT, swapArray[i], 1, MPI_COMM_WORLD);
+                
+                
+            }
+        }
+        
+        //wait until all trajectories have gotten this far before waiting for information (mostly for printing purposes)
+        MPI_Barrier(MPI_COMM_WORLD);
+        
+        //recv your swap partner
+        int mySwapper;
+        int head_rank = 0;
+        MPI_Recv(&mySwapper,1, MPI_INT, head_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //cout << "Rank " << my_rank << " recv swapper " << (mySwapper) << endl;
+        
+        //recv your parent child status
+        int parent_or_child;
+        MPI_Recv(&parent_or_child,1, MPI_INT, head_rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //cout << "Rank " << my_rank << " parent_or_child " << parent_or_child   <<endl;
+        //wait until all trajectories have finished this traj before continuing and trying to do the swaps.
+        
+        //lets wait for good measure
+        MPI_Barrier(MPI_COMM_WORLD);
+        
+        //there are two kind of processes, children and parents.  children send info to parents
+        //parents actually compute rejection prob
+        //parents return the value of field to child (which may be same as original field!)
+        
+        if (parent_or_child == 1) //child
+        {
+            //send your info to the parent
+            double myK[1];
+            *myK = (double)trajectory.GetOrderParameter();
+            MPI_Send(&myK,1, MPI_DOUBLE, mySwapper, 2, MPI_COMM_WORLD);
+            
+            double myS[1];
+            *myS = (myTPS.GetCurrS());
+            MPI_Send(&myS,1, MPI_DOUBLE, mySwapper, 3, MPI_COMM_WORLD);
+            cout << my_rank << " sent to " << mySwapper << " K = " << *myK << " S = " << *myS << endl;
+            
+            
+            //wait for your new value of S (might be your current value!)
+            double swapS;
+            MPI_Recv(&swapS,1, MPI_DOUBLE, mySwapper, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            myTPS.SetS(swapS);
+            //cout << "my_rank " << my_rank << " current S " << swapS << endl;
+            
+            
+        }
+        
+        if(parent_or_child == 0) //parent
+        {
+            //wait for information from child
+            double swapK;
+            double swapS;
+            MPI_Recv(&swapK,1, MPI_DOUBLE, mySwapper, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&swapS,1, MPI_DOUBLE, mySwapper, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            //cout << my_rank << " recv from " << mySwapper << " K = " << swapK << " S = " << swapS << endl;
+            
+            //do some metropolis stuff
+            double deltaK = (double)trajectory.GetOrderParameter()-swapK;
+            double deltaS = myTPS.GetCurrS() - swapS;
+            //cout << "deltaK " << deltaK << " delta S " << deltaS << endl;
+            
+            double boltz = exp(deltaK*deltaS);
+            //cout << "botlz " << boltz << endl;
+            //cout << "my_rank " << my_rank << " original S " << myTPS.GetCurrS() << endl;
+            
+            double randNum = ((double)rand()/(double)RAND_MAX);
+            //cout << "botlz " << boltz << " rand num " << randNum << endl;
+            if (randNum < boltz) //randnum guarantteed to be less than 1
+            { //accept
+                double myS[1];
+                *myS = (myTPS.GetCurrS());
+                MPI_Send(&myS,1, MPI_DOUBLE, mySwapper, 4, MPI_COMM_WORLD);
+                myTPS.SetS(swapS); //change my S to the swap value of S
+                
+            }
+            else
+            {  //reject
+                //send back the original value of S.
+                double originalS[1];
+                *originalS = swapS;
+                MPI_Send(&originalS,1, MPI_DOUBLE, mySwapper, 4, MPI_COMM_WORLD);
+            }
+            
+            //cout << "my_rank " << my_rank << " current S " << (myTPS.GetCurrS()) << endl;
+            
+        }
+        /***********************MPI*************************/
+        
 		seed++;//increments the seed
 		srand(seed); //reseeds the rand num gen
 		if (i%m_n_storagefreq == 0)  //prints a checkpoint/restart file every "m_n_storagefreq" amt
